dfa3018df3cf61276de3d45210dc9cf624f99cd1
Pablo Irineu <pablo.souza.5@outlook.com.br>
2024-11-11 05:16:38 -0300

    adicionar formatador de cep

--- Utils/StringFormatter.cs
+++ Utils/StringFormatter.cs
@@ -1,6 +1,6 @@
 ﻿namespace EventFlow.Utils
 {
-    public class TelefoneFormatter
+    public class StringFormatter
     {
         public static string FormatarTelefone(string telefone)
         {
@@ -8,6 +8,14 @@
                 return telefone;
 
             return $"({telefone.Substring(0, 2)}) {telefone.Substring(2, 5)}-{telefone.Substring(7, 4)}";
+        }
+
+        public static string FormatarCEP(string cep)
+        {
+            if (string.IsNullOrEmpty(cep) || cep.Length != 8)
+                return cep;
+
+            return $"{cep.Substring(0, 5)}-{cep.Substring(5, 3)}";
         }
     }
 }
